c389d830b82cb1c52d90e9c5fd037eda81142817
hieucronc <21020318@vnu.edu.vn>
2022-10-18 23:19:03 +0700

    Ballom dead animation

--- src/uet/oop/bomberman/entities/dynamic/Ballom.java
+++ src/uet/oop/bomberman/entities/dynamic/Ballom.java
@@ -3,6 +3,7 @@ package uet.oop.bomberman.entities.dynamic;
 import javafx.scene.image.Image;
 import uet.oop.bomberman.controller.Movement;
 import uet.oop.bomberman.graphics.Sprite;
+
 import static uet.oop.bomberman.BombermanGame.*;
 import static uet.oop.bomberman.entities.blocks.Bomb.decayTimer;
 
@@ -17,9 +18,11 @@ public class Ballom extends DynamicEntities {
     private static final int DOWN = 1;
     private static final int LEFT = 2;
     private static final int RIGHT = 3;
+
     public Ballom(int x, int y, Image img) {
-        super(x,y,img);
+        super(x, y, img);
     }
+
     public void ballomMove() {
 //        if (dir == UP && !Movement.canMoveUp(this) && tick <= 70) {
 //            while (dir == UP) {
@@ -92,10 +95,11 @@ public class Ballom extends DynamicEntities {
     }
 
     public void killBallom() {
-        this.setImg(Sprite.movingSprite(Sprite.balloom_dead,Sprite.balloom_dead,Sprite.balloom_dead,countBallomDead,60).getFxImage());
+        this.setImg(Sprite.movingSprite(Sprite.balloom_dead,
+                Sprite.mob_dead1, Sprite.mob_dead2, countBallomDead, decayTimer).getFxImage());
         countBallomDead++;
-        if (countBallomDead > decayTimer)  {
-            for (int i=0;i<enemy.size();i++) {
+        if (countBallomDead > decayTimer) {
+            for (int i = 0; i < enemy.size(); i++) {
                 if (enemy.get(i).getX() == this.getX() && enemy.get(i).getY() == this.getY()) {
                     enemy.remove(i);
                 }
