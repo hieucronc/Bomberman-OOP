4883c89aaffa30d7c81b5201a47cdd77a487e8f7
hieucronc <21020318@vnu.edu.vn>
2022-10-26 02:21:09 +0700

    Monster and Item introducing page

--- src/uet/oop/bomberman/controller/Menu.java
+++ src/uet/oop/bomberman/controller/Menu.java
@@ -22,28 +22,35 @@ import static uet.oop.bomberman.sound.SoundManager.buttonSound;
 import static uet.oop.bomberman.sound.SoundManager.titleScreen;
 
 public class Menu {
-    public static Text level, bomb, time;
-    public static ImageView statusGame, newGameButton, exitButton, backgroundMenu;
-    public static Image resumeGame, pauseGame;
-    public static int bomb_number = 20, time_number = 120;
-
+    public static ImageView backButton, newGameButton, exitButton, backgroundMenu;
+    public static ImageView itemPage, itemButton, monsterButton;
+    public static ImageView enemyPage;
     //    public static Pane pane;
-    static Image newGameImage = new Image("file:res/textures/start.png");
+    static Image backgroundMenuImage = new Image("file:res/textures/Name.png");
     static Image exitGameImage = new Image("file:res/textures/exit.png");
+    static Image itemImage = new Image("file:res/textures/itempage.png");
+    static Image enemyImage = new Image("file:res/textures/enemypage.png");
+    static Image backImage = new Image("file:res/textures/back.png");
+    static Image newGameImage = new Image("file:res/textures/start.png");
+    static Image enemyButtonImage = new Image("file:res/textures/monsterbutton.png");
+    static Image itembuttonImage = new Image("file:res/textures/itembutton.png");
+
 
     public static void createMenu(Group root) throws UnsupportedAudioFileException, LineUnavailableException, IOException {
 
         if (!running) {
             titleScreen();
-            Image backgroundMenuImage = new Image("file:res/textures/Name.png");
             backgroundMenu = new ImageView(backgroundMenuImage);
             backgroundMenu.setX(0);
             backgroundMenu.setY(0);
-//            backgroundMenu.setScaleX(0.05);
-//            backgroundMenu.setScaleY(0.05);
 
             exitButton = new ImageView(exitGameImage);
             newGameButton = new ImageView(newGameImage);
+            backButton = new ImageView(backImage);
+            enemyPage = new ImageView(enemyImage);
+            itemPage = new ImageView(itemImage);
+            itemButton = new ImageView(itembuttonImage);
+            monsterButton = new ImageView(enemyButtonImage);
 
             newGameButton.setX(320);
             newGameButton.setY(250);
@@ -54,11 +61,28 @@ public class Menu {
             exitButton.setY(305);
             exitButton.setScaleX(0.2);
             exitButton.setScaleY(0.2);
+
+            backButton.setX(855);
+            backButton.setY(-75);
+            backButton.setScaleX(0.2);
+            backButton.setScaleY(0.2);
+
+            itemButton.setX(801);
+            itemButton.setY(70);
+
+            monsterButton.setX(804);
+            monsterButton.setY(178);
+
             root.getChildren().add(backgroundMenu);
             root.getChildren().add(newGameButton);
             root.getChildren().add(exitButton);
+            root.getChildren().add(monsterButton);
+            root.getChildren().add(itemButton);
+            root.getChildren().add(enemyPage);
+            root.getChildren().add(itemPage);
+            root.getChildren().add(backButton);
+            displayMenu();
         }
-
 
 
         AnimationTimer timer = new AnimationTimer() {
@@ -67,8 +91,10 @@ public class Menu {
                 newGameButton.setOnMouseClicked(event -> {
                     running = true;
                     backgroundMenu.setVisible(false);
-                    newGameButton.setVisible(false);
                     exitButton.setVisible(false);
+                    monsterButton.setVisible(false);
+                    itemButton.setVisible(false);
+                    newGameButton.setVisible(false);
                     try {
                         buttonSound();
                     } catch (UnsupportedAudioFileException e) {
@@ -88,16 +114,77 @@ public class Menu {
                 exitButton.setOnMouseClicked(event -> {
                     Platform.exit();
                 });
-//                statusGame.setOnMouseClicked(event -> {
-//                    pausing = !pausing;
-//                    running = !running;
-//                });
-//                updateMenu();
+                backButton.setOnMouseClicked(event -> {
+                    displayMenu();
+                    try {
+                        buttonSound();
+                    } catch (UnsupportedAudioFileException e) {
+                        throw new RuntimeException(e);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    } catch (LineUnavailableException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+                itemButton.setOnMouseClicked(event -> {
+                    displayItem();
+                    try {
+                        buttonSound();
+                    } catch (UnsupportedAudioFileException e) {
+                        throw new RuntimeException(e);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    } catch (LineUnavailableException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+                monsterButton.setOnMouseClicked(event -> {
+                    displayMonster();
+                    try {
+                        buttonSound();
+                    } catch (UnsupportedAudioFileException e) {
+                        throw new RuntimeException(e);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    } catch (LineUnavailableException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+
             }
         };
         timer.start();
     }
-
+    public static void displayMenu() {
+        backgroundMenu.setVisible(true);
+        exitButton.setVisible(true);
+        monsterButton.setVisible(true);
+        itemButton.setVisible(true);
+        newGameButton.setVisible(true);
+        itemPage.setVisible(false);
+        enemyPage.setVisible(false);
+        backButton.setVisible(false);
+    }
+    public static void displayItem() {
+        backgroundMenu.setVisible(false);
+        exitButton.setVisible(false);
+        monsterButton.setVisible(false);
+        itemButton.setVisible(false);
+        newGameButton.setVisible(false);
+        enemyPage.setVisible(false);
+        itemPage.setVisible(true);
+        backButton.setVisible(true);
+    }
+    public static void displayMonster() {
+        backgroundMenu.setVisible(false);
+        exitButton.setVisible(false);
+        monsterButton.setVisible(false);
+        itemButton.setVisible(false);
+        newGameButton.setVisible(false);
+        itemPage.setVisible(false);
+        enemyPage.setVisible(true);
+        backButton.setVisible(true);
+    }
 
 }
 
